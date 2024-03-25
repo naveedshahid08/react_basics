716161bc44c9abd48e713531029e1c2ea95b8823
Naveed Shahid <naveedshahid08@gmail.com>
2024-03-25 09:47:19 +0200

    updating

---
+++ 01basics/src/App.jsx
@@ -0,0 +1,19 @@
+import Chai from "./chai";
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Chai />
+      <h3>Chai is hot</h3>
+      <p>Chai is sweet</p>
+      <p>Chai is sweet</p>
+    </>
+    
+  );
+}
+
+export default App
