ba7d9bb4c62654f4cbdd28bd69da64d111b70393
Naveed Shahid <naveedshahid08@gmail.com>
2024-03-25 14:56:46 +0200

    updating class code

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -1,18 +1,55 @@
-import './App.css'
-import Box from './Box'
-
-
+import "./App.css";
+import Box from "./Box.jsx";
+import { useState } from "react";
 
 function App() {
- 
+  /* const [counter, setCounter] = useState("Hello"); */
+  const [counter, setCounter] = useState(0);
+
+  const [persons, setPersons] = useState([
+    { id: 1, name: "Naveed", title: "Student", location: "Helsinki" },
+    { id: 2, name: "Smith", title: "Teacher", location: "Espoo" },
+    { id: 3, name: "Jhon", title: "Master", location: "Vantaa" },
+  ]);
+
+  const clickHandler = () => {
+    setCounter(counter + 1);
+    /* console.log("Button Clicked"); */
+  };
+  const decreaseButton = () => {
+    setCounter(counter - 1);
+  };
+  const defaultButton = () => {
+    setCounter(0);
+  };
+
   return (
     <>
-      <Box />
-      <Box />
-      <Box/>
+      <p>Counter:{counter}</p>
+      <button onClick={clickHandler}>Increase Button +</button>
+      <button onClick={decreaseButton}>Decrease Button -</button>
+      <button onClick={defaultButton}>Set 0 </button>
+
+      {/* <Box name="Naveed" title="Student" location="Helsinki" />
+   /* These are just place holders
+      <Box name="Nav" title="Writer" location="Estonia"/>
+      <Box name="Will" title="Driver" location="USA" /> */}
+      {persons.map((person) => (
+        <Box
+          key={person.id}
+          name={person.name} //We are using here the code from the array into the Box Component
+          title={person.title}
+          location={person.location}
+        />
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
+{
+  /* <p>{person.name}</p> // Here is how to display data from the array without Component.
+            <p>{person.title}</p>
+            <p>{person.location}</p> */
+}
--- react-demo/src/Box.jsx
+++ react-demo/src/Box.jsx
@@ -1,10 +1,10 @@
-
-const Box = () => {
+const Box = (props) => {
+  return (
     <div>
-<h1>Name:</h1>
-<p>Title:</p>
-<p>Location:</p>
-</div>
-  
-}
-export default Box
+      <h1>Name:{props.name}</h1>
+      <p>Title:{props.title}</p>
+      <p>Location:{props.location}</p>
+    </div>
+  );
+};
+export default Box;
