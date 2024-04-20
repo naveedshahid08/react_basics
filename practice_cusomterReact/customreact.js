8a39ee8fc84b5477a97d19925b1768cad38ab08f
Naveed Shahid <naveedshahid08@gmail.com>
2024-04-21 00:07:36 +0300

    deleted counter

---
+++ practice_cusomterReact/customreact.js
@@ -0,0 +1,22 @@
+const mainContainer = document.querySelector("#root");
+
+function customRender(reactElement, container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    if (prop === "childern") continue;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  container.appendChild(domElement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "click me to visit google",
+};
+
+customRender(reactElement, mainContainer);
